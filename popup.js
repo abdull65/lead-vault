f7acf3cf39835841279030211e90f9b873eea498
Abdullahi Mohammed <odanwu211@Gmail.com>
2025-10-26 14:10:00 +0100

    feat: added copy to clipboard for saved leads

--- popup.js
+++ popup.js
@@ -15,7 +15,19 @@ points="3 6 5 6 21 6">
 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 
 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path>
 <line x1="10" y1="11" x2="10" y2="17"></line>
-<line x1="14" y1="11" x2="14" y2="17"></line></svg>`
+<line x1="14" y1="11" x2="14" y2="17"></line></svg>`;
+
+const copyBtn = `<svg xmlns="http://www.w3.org/2000/svg" 
+width="12" height="12" viewBox="0 0 24 24" fill="none" 
+stroke="#0078d7" stroke-width="2" stroke-linecap="round"
+stroke-linejoin="round" class="feather feather-copy"><rect x="9" y="9" 
+width="13" height="13" rx="2" ry="2"></rect><path 
+d="M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1"></path></svg>`;
+
+const checkIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" 
+viewBox="0 0 24 24" fill="none" stroke="green" stroke-width="2" 
+stroke-linecap="round" stroke-linejoin="round" 
+class="feather feather-check"><polyline points="20 6 9 17 4 12"></polyline></svg>`;
 
 
 searchInput.addEventListener('input', e => {
@@ -49,7 +61,10 @@ function renderLeads(data = leads) {
     <p>
     <strong>${lead.title || 'Lead'}</strong>
     </p>
+    <div class="action-btn-div">
+    <button class="copyBtn" data-index="${index}">${copyBtn}</button>
     <button class="deleteBtn" data-index="${index}">${deleteBtn}</button>
+    </div>
     </div>
     <div class="lead-text">
     ${lead.url ? `<a href="${lead.url}" target="_blank">${lead.url}</a><br>` : ''}
@@ -67,6 +82,21 @@ function renderLeads(data = leads) {
       renderLeads();
     });
   });
+  document.querySelectorAll('.copyBtn').forEach(btn => {
+    btn.addEventListener('click', e => {
+      const index = e.currentTarget.dataset.index;
+      const lead = leads[index];
+      if(!lead) return;
+      const textToCopy = lead.url 
+      ? `${lead.url}`
+      : `${lead.text || ''}`;
+
+      navigator.clipboard.writeText(textToCopy).then(() => {
+        btn.innerHTML = checkIcon;
+        setTimeout(() => (btn.innerHTML = copyBtn), 1500);
+      }) .catch(err => console.error('failed to copy: ', err));
+    })
+  })
 }
 
 
